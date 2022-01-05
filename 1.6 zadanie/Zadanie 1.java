8029401a103a601c75af5416e93a44573c2740ed
Szymon Kurzynski <you@example.com>
2022-01-05 09:35:53 +0100

    Zadanie 1.6

---
+++ 1.6 zadanie/Zadanie 1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Zadanie1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        final String stalehaslo = haslo123;
+        String haslo = "";
+        System.out.println("Podaj hasło: ");
+        haslo = scanner.next();
+        if(haslo.equals(stalehaslo))
+        {
+            System.out.println("Twoje hasło jest poprawne");
+        }
+        else
+        {
+            System.out.println("Twoje hasło jest niepoprawne");
+        }
+    }
+  }
---
+++ 1.6 zadanie/Zadanie 3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Zadanie3 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String imie = "";
+        String nazwisko = "";
+        int wiek = 0;
+        int waga = 0;
+        float wzrost = 0;
+    }
+  }
