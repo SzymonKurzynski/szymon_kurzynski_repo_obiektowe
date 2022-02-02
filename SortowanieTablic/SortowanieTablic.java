bfaebb7b46e56e8284b3a17c6a469290a4830310
Szymon Kurzynski <you@example.com>
2022-02-02 09:29:44 +0100

    SortowanieTablic

--- SortowanieTablic/SortowanieTablic.java
+++ SortowanieTablic/SortowanieTablic.java
@@ -1,17 +1,24 @@
+import java.util.Arrays;
 import java.util.Random;
-final int ILOSC_ELEMENTOW = 1000000;
+
 public class SortowanieTablic {
    public static void main(String[] args){
        
-        Random r = new Random();
-        int [] tab = new int[ILOSC_ELEMENTOW];
-
-
+        final int ILOSC_ELEMENTOW = 100000;
+        Random r = new Random();  // do losowania liczb
+        long millisActualTime = 0, executionTime = 0;
+        for(int k=0;k<10;k++){
+        int[] tabBubble = new int[ILOSC_ELEMENTOW];  // moja tablica
+        // wypełnianie losowymi elementami mojej tablicy
         for(int i=0;i<ILOSC_ELEMENTOW;i++){
-           tab[i] = r.nextInt(1000000)+1;
+           tabBubble[i] = r.nextInt(1000000)+1;  // losowe liczby od 1 do 1000000
        }
-        System.out.println("Przed sortowaniem");
-        long millisActualTime - System.currentTimeMillis();
+       int[] tabQuick = Arrays.copyOf(tabBubble, tabBubble.lenght);
+       millisActualTime = System.currentTimeMillis();
+       Arrays.sort(tabQuick);  // sortowanie Quicksort wbudowane w JAVE
+       executionTime = System.currentTimeMillis() - millisActualTime;
+       System.out.println("Sortowanie Quicksort czas wykonania : "+ executionTime);
+
         //SORTOWANIE BĄBELKOWE
         for (int i = 0; i<tab.length; i++){
             for (int j = 1; j< tab.length; j++){
@@ -20,12 +27,14 @@ public class SortowanieTablic {
                     tab[j - 1] = tab[j];
                     tab[j] = wieksza;
  
+                    }
+
                 }
 
-            }
-
-        } 
-        long executionTime = System.currentTimeMillis() - millisActualTime;
-        System.out.println("Sortowanie bąbelkowe czas wykonania: "+executionTime);
+            } 
+            long executionTime = System.currentTimeMillis() - millisActualTime;
+            System.out.println("Sortowanie bąbelkowe czas wykonania: "+executionTime);
+   
+        }
     }
 }
